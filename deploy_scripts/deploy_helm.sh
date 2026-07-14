f8f7973c5ea611843f0ff1d7fee8d165a12cbbca
storm <storm@stormdeMacBook-Air.local>
2026-07-14 13:48:56 +0800

    调大资源配置

--- deploy_scripts/deploy_helm.sh
+++ deploy_scripts/deploy_helm.sh
@@ -1,1 +1,14 @@
-helm upgrade --install superset ./helm/superset -n bigdata -f ./helm/superset/baidu-k8s-values.yaml
+#!/bin/sh
+set -e
+
+helm dependency build ./helm/superset
+helm upgrade --install superset ./helm/superset \
+  -n bigdata \
+  -f ./helm/superset/baidu-k8s-values.yaml \
+  --set-string "supersetNode.podAnnotations.deploy-timestamp=$(date +%s)" \
+  --wait \
+  --timeout 20m
+
+echo "Waiting for rollout to complete..."
+kubectl rollout status deployment/superset -n bigdata --timeout=20m
+echo "Deploy finished."
